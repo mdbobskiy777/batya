a095b10e5eda9505569e70c72f633c60fbe5fdb4
mdb_art <mdbobskiy777@gmail.com>
2020-08-03 13:23:02 +0300

    added base template of app

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+
+    return (
+        <div className = 'navBar'>
+            <div>1 menu</div>
+            <div>2 menu</div>
+            <div>3 menu</div>
+            <div>4 menu</div>
+        </div>
+    )
+}
+export default NavBar;
---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Profile = () => {
+
+    return (
+        <div className = 'profile'>
+            <div>
+                <img src={"https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png"}/>
+            </div>
+            <div>info</div>
+        </div>
+    )
+}
+export default Profile;
